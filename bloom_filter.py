10840569027cebe777201893c1aab4de81489352
Clare-Joyce <joycetirnyuy@gmail.com>
2024-05-27 19:12:49 +0200

    Add project implementation outline

---
+++ bloom_filter.py
@@ -0,0 +1,24 @@
+
+
+
+class BloomFilter:
+
+    def __init__(self) -> None:
+        pass
+    pass
+
+
+    def insert(self):
+        pass
+
+
+    def check(self):
+        pass
+
+
+    def hash_function(self):
+        pass
+
+    
+    def calculate_no_hash_functions(self):
+        pass
