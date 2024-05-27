44c0e14cd9e08b2bfc6545e3d0cfce1d2c8d20d1
Clare-Joyce <joycetirnyuy@gmail.com>
2024-05-27 19:27:04 +0200

    Add description of methods

--- bloom_filter.py
+++ bloom_filter.py
@@ -4,15 +4,18 @@
 class BloomFilter:
 
     def __init__(self) -> None:
+        """Initialization."""
         pass
     pass
 
 
     def insert(self):
+        """Adds an element to the set."""
         pass
 
 
     def check(self):
+        """Checks if an element exists in the set."""
         pass
 
 
@@ -21,4 +24,5 @@ class BloomFilter:
 
     
     def calculate_no_hash_functions(self):
+        """Calculates the optimal number of hash functions."""
         pass
