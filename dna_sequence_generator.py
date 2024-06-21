e59aed9a5b3e34d4e94cfe8def9bc4ed8e12dd39
Clare-Joyce <joycetirnyuy@gmail.com>
2024-06-21 16:41:29 +0200

    Add a random dna sequence generator

---
+++ dna_sequence_generator.py
@@ -0,0 +1,27 @@
+"""Generates random DNA sequences."""
+
+import random
+
+def generate_dna_sequence(length: int) -> str:
+    """Generates a random DNA sequence of a given length.
+
+    Args:
+        length (int): The length of the DNA sequence to generate.
+
+    Returns:
+        A string representing a random DNA sequence composed of 'A',
+        'T', 'C', 'G'.
+    """
+    return ''.join(random.choice(['A', 'T', 'C', 'G']) for _ in range(length))
+
+def generate_multiple_dna_sequences(num_sequences: int, length: int) -> list:
+    """Generates several random DNA sequences.
+
+    Args:
+        num_sequences (int): Number of DNA sequences to generate.
+        length (int): The length of each DNA sequence.
+
+    Returns:
+        list: A list of random DNA sequences.
+    """
+    return [generate_dna_sequence(length) for _ in range(num_sequences)]
