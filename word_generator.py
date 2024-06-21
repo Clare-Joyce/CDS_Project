0bc6dfc1970d6be16f6b959cff2dab51e3891270
SARIGA KAKKAMANI <77626301+Sarigakakkamani@users.noreply.github.com>
2024-06-21 16:51:41 +0200

    Add function to generate random words

--- word_generator.py
+++ word_generator.py
@@ -1,3 +1,16 @@
-"""
-Generates random words for testing
-"""
+"""Generates random words for testing."""
+from faker import Faker
+
+fake = Faker()
+
+def random_word_generator(n):
+    """Generates a random set of words.
+
+    Args:
+        n (int): The number of random uniform words to be generated.
+
+    Returns:
+        random_words (list): Returns a list of random words
+    """
+    random_words = [fake.word() for _ in range(n)]
+    return random_words
