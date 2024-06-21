4776d90df6d07d57e54781cfca4f5cc43ad73c16
Clare-Joyce <joycetirnyuy@gmail.com>
2024-06-21 12:54:55 +0200

    Add object initialization parameters

--- bloom_filter.py
+++ bloom_filter.py
@@ -1,21 +1,32 @@
-
+from typing import Any
 
 
 class BloomFilter:
 
-    def __init__(self) -> None:
-        """Initialize the bloom filter."""
-        pass
-    pass
+    def __init__(self, capacity: int, false_positive_rate: float) -> None:
+        """Initialize the bloom filter.
+
+        Args:
+            capcacity (int): Number of elements the filter can hold
+            num_hash_functions (int): Number of hash functions to use
+
+        Returns:
+            None.
+        """
+        # Define the filter capacity
+        self.capacity = capacity
+        self.false_positive_rate = false_positive_rate
+        # Set all cells to False
+        self.bit_array = [False]  * capacity
 
 
-    def insert(self):
-        """Adds an element to the set."""
+    def insert(self, item):
+        """Adds an element to the filter."""
         pass
 
 
     def check(self):
-        """Checks if an element exists in the set."""
+        """Checks if an element exists in the filter."""
         pass
 
 
