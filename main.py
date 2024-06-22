a2ed5fc4ccac0a572c30ff182ed735045c0425ab
Clare-Joyce <joycetirnyuy@gmail.com>
2024-06-22 15:08:02 +0200

    Modify the process function

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ import math
 import time
 import pandas as pd
 from bloom_filter import BloomFilter
-from dna_sequence_generator import generate_multiple_dna_sequences
 from word_generator import random_word_generator
+from dna_sequence_generator import generate_multiple_dna_sequences
+from independence_and_uniformity import chi_square_test
 
 
 def parse_arguments():
     """Parses command line arguments."""
     parser = argparse.ArgumentParser(
         description="Bloom Filter Benchmarking Tool")
-    parser.add_argument("--capacity", default=100, type=int, required=True,
+    parser.add_argument("--capacity", default=100, type=int, required=False,
                         help="Maximum number of elements the filter can hold.")
-    parser.add_argument("--fpr", default=0.01, type=float, required=True,
+    parser.add_argument("--fpr", default=0.01, type=float, required=False,
                         help="Initial desired false positive rate.")
     parser.add_argument("--m", default=0, type=int, required=False,
                         help="Size of the bit array")
@@ -33,8 +34,7 @@ def parse_arguments():
 
 
 def calculate_compression_rate(m: int, n: int) -> float:
-    """
-Calculate the compression rate of a Bloom filter.
+    """Calculate the compression rate of a Bloom filter.
 
     Args:
         m (int): Size of the bit array (number of bits).
@@ -73,8 +73,7 @@ def calculate_false_positive_rate(m: int, k: int, n: int) -> float:
     return false_positive_rate
 
 
-def process(cap: int, fpr: float, m: int, k: int, data_type: str,
-            seq_len: int) -> pd.DataFrame:
+def process() -> pd.DataFrame:
     """Processes bloom filter functionality and tests performance.
 
     Note:
@@ -82,48 +81,57 @@ def process(cap: int, fpr: float, m: int, k: int, data_type: str,
         specify just the capacity and the false positive rate or
         specify the bit array size and the number of hash functions
         needed. For the first case, the program automatically calculates
-        the optimal bit array size and the optimal number of hash functions
+        the optimal bit array size and the optimal number of hash functions.
 
     Args:
         cap (int): The initial capacity of the filter
         fpr (float): The false positive rate defined by the user
         m (int): The bit array size
         k (int): The desired number of hash functions
-        data_type (str): The data type, normal words or DNA sequences
+        data_type (str): The data type; normal words or DNA sequences
         seq_len (int): The length of the DNA sequence
 
     Returns:
         A pandas dataframe
     """
-    results = []
-    for i in range(1, cap, 5):
-        bf = BloomFilter(i, fpr, m, k)
-        if data_type == "dna":
-            items = generate_multiple_dna_sequences(i, seq_len)
-        if data_type == "words":
-            items = random_word_generator(i)
-        start_time = time.time()
-        for item in items:
-            bf.insert(item)
-        insertion_time = time.time() - start_time
 
-        start_time = time.time()
-        for item in items:
-            bf.check(item)
-        checking_time = time.time() - start_time
+    args = parse_arguments()
+    # input_size = [i for i in range(10000, 1000001, 10000)]
+    input_size = [i for i in range(100, 1000, 100)]
+    for k in range(5, args.k + 1, 5):
+        results = []
+        for i in input_size:
+            bf = BloomFilter(args.capacity, args.fpr, args.m, k)
+            if args.data_type == "dna":
+                sl = args.sequence_length
+                items_in = generate_multiple_dna_sequences(i, sl)
+                items_check = generate_multiple_dna_sequences(i, sl)
+            if args.data_type == "words":
+                items_in = random_word_generator(i)
+                items_check = random_word_generator(i)
 
-        fpr_new = calculate_false_positive_rate(bf.m, bf.k, i)
-        cpr = calculate_compression_rate(bf.m, i)
-        results.append((i, insertion_time, checking_time, fpr_new, cpr))
-    df = pd.DataFrame(results, columns=["capacity", "insertion_time",
-                                        "checking_time", "fpr", "cpr"])
-    df.to_csv(f"dataframe_{data_type}_{cap}_{k}.csv")
+            start_time = time.time()
+            for item in items_in:
+                bf.insert(item)
+            insertion_time = time.time() - start_time
+
+            start_time = time.time()
+            for item in items_check:
+                bf.check(item)
+            checking_time = time.time() - start_time
+
+            fpr_new = calculate_false_positive_rate(bf.m, bf.k, i)
+            cpr = calculate_compression_rate(bf.m, i)
+            uniform, independent = chi_square_test(bf, items_in)
+            results.append((i, insertion_time, checking_time, fpr_new,
+                            cpr, uniform, independent))
+        df = pd.DataFrame(results, columns=["input_size", "insertion_time",
+                                            "checking_time", "fpr", "cpr",
+                                            "uniform", "independent"])
+        df.to_csv(f"dataframe_{args.data_type}_{k}.csv")
 
     return df
 
 
 if __name__ == "__main__":
-    args = parse_arguments()
-    for k in range(5, args.k, 5):
-        process(args.capacity, args.fpr, args.m, k,
-                args.data_type, args.sequence_length)
+    process()
